a2f20e816f420dc7f8f2b47292e541f02e0fb8a1
Loren Mora <loren.mora@ncf.edu>
2021-02-08 14:50:29 -0800

    add cors proxy

--- src/components/searchMoviesComponent.js
+++ src/components/searchMoviesComponent.js
@@ -11,7 +11,7 @@ export default function SearchMovies() {
 
     const searchMoviesBechdel = async () => {
 
-        const url = `http://bechdeltest.com/api/v1/getMoviesByTitle?title=${query}`;
+        const url = `https://cors-proxy.htmldriven.com/?url=http://bechdeltest.com/api/v1/getMoviesByTitle?title=${query}`;
 
         try {
             const res = await fetch(url);
