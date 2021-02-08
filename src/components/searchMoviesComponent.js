a648677a63d9576495d317f2a7a909ea82120131
Loren Mora <64873256+LorenMora@users.noreply.github.com>
2021-02-08 14:13:04 -0800

    Update search searchMoviesComponent
    Deleted CORS anywhere

--- src/components/searchMoviesComponent.js
+++ src/components/searchMoviesComponent.js
@@ -11,7 +11,7 @@ export default function SearchMovies() {
 
     const searchMoviesBechdel = async () => {
 
-        const url = `https://cors-anywhere.herokuapp.com/http://bechdeltest.com/api/v1/getMoviesByTitle?title=${query}`;
+        const url = `http://bechdeltest.com/api/v1/getMoviesByTitle?title=${query}`;
 
         try {
             const res = await fetch(url);
