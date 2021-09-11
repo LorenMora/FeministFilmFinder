507171fddff9e6e6c556003fc8ccb17091923b81
Loren Mora <loren.mora@ncf.edu>
2021-09-11 12:41:44 -0700

    added loader

--- src/components/searchMoviesComponent.js
+++ src/components/searchMoviesComponent.js
@@ -2,19 +2,22 @@ import React, { useState } from "react";
 import BechdelContainer from "./bechdelContainer";
 
 export default function SearchMovies() {
+  const [loading, isLoading] = useState(false);
   const [query, setQuery] = useState("");
-
   const [moviesBechdel, setMoviesBechdel] = useState([]);
 
   const searchMoviesBechdel = async () => {
     const url = `https://gentle-atoll-20187.herokuapp.com/http://bechdeltest.com/api/v1/getMoviesByTitle?title=${query}`;
 
     try {
+      isLoading(true);
       const res = await fetch(url);
       const data = await res.json();
       setMoviesBechdel(data);
     } catch (err) {
       console.error(err);
+    } finally {
+      isLoading(false);
     }
   };
 
@@ -41,6 +44,13 @@ export default function SearchMovies() {
           Submit
         </button>
       </form>
+      {loading && (
+        <img
+          className="loader"
+          src="images/spinner.gif"
+          alt="loading spinner"
+        />
+      )}
       <section className="card-list">
         {moviesBechdel.map((movie) => (
           <BechdelContainer
